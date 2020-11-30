c6a30019a1cd7f08f0ca2e360ccdc292ea887e2b
Coline ALBERTI <coline.alberti@gmail.com>
2020-11-30 12:50:17 +0100

    refactor code

--- js/alcoolemie.js
+++ js/alcoolemie.js
@@ -1,3 +1,11 @@
+/**
+ * Fonction qui retourne l'alcool pur intégré en fonction du nombre
+ * de verre
+ * 
+ * @param {int} nbVerres
+ * @return {int}
+ */
 function getAlcoolPur(nbVerres) {
-    return nbVerres * 10;
+    const uniteAlcool = 10;
+    return uniteAlcool * nbVerres;
 }
