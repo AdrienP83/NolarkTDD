fadcb59fb093d554684eb04bbfb64ae04c7e0cff
José GIL <jgil83000@gmail.com>
2020-11-30 12:33:40 +0100

    fonction getAlcoolPur créer et test passé

--- js/alcoolemie.js
+++ js/alcoolemie.js
@@ -3,5 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/**
+ * Projet alcoolémie Nolark dans le but de sensibilisé les utilisaeur sur l'accolémie
+ * Created by Adrien Poignard étudiant en BTS SIO à Toulon
+ * mail: poignard.adrien@gmail.com
+ */
 
 
+/**
+ * Fonction qui retourne l'accol pur ingérer en fonction du nombre 
+ * de verres
+ * @param {int} nbVerres
+ * @return {int}
+ */
+function getAlcoolPur(nbVerres){
+    const unitAlcool = 10;
+    return nbVerres * unitAlcool;
+}
--- test/unit/testAlcoolemie.js
+++ test/unit/testAlcoolemie.js
@@ -8,5 +8,9 @@
  * Created by Adrien Poignard étudiant en BTS SIO du Lycée Bonaparte à Toulon
  * email: poignard.adiren@gmail.com
  */
-
+MesTestUnitaires = TestCase('AlcoolemieTest');
+MesTestUnitaires.prototype.testsGetAlcoolPur = function(){
+    assertEquals('0 verre',0,getAlcoolPur(0));
+    assertEquals('1 verre',10,getAlcoolPur(1));
+}
 
