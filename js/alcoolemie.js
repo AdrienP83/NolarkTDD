432a61f3716093fbe068248f6a3134ac9c69c884
Coline ALBERTI <coline.alberti@gmail.com>
2020-12-07 08:14:22 +0100

    fonction getCoefDiffusion

--- js/alcoolemie.js
+++ js/alcoolemie.js
@@ -9,3 +9,18 @@ function getAlcoolPur(nbVerres) {
     const uniteAlcool = 10;
     return uniteAlcool * nbVerres;
 }
+
+/**
+ * Fonction qui retourne le coefficient de diffusion en fonction du sexe
+ * 
+ * @param {string} sexe
+ * @return {float}
+ */
+function getCoefDiffusion(sexe){
+    const coefDiffuH = 0.7, coefDiffuF = 0.6
+    if (sexe === 'homme'){
+        return coefDiffuH;
+    } else{
+        return coefDiffuF;
+    }
+}
--- test/unit/testAlcoolemie.js
+++ test/unit/testAlcoolemie.js
@@ -6,4 +6,8 @@ MesTestsUnitaires = TestCase('AlcoolemieTest');
 MesTestsUnitaires.prototype.testsGetAlcoolPur = function(){
     assertEquals('0 verre', 0, getAlcoolPur(0));
     assertEquals('1 verre', 10, getAlcoolPur(1));
-}
+};
+MesTestsUnitaires.prototype.testsGetCoefDiffusion = function() {
+    assertEquals('Homme', 0.7, getCoefDiffusion('homme'));
+    assertEquals('Femme', 0.6, getCoefDiffusion('femme'));
+};
