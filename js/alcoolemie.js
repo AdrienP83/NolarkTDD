946103a58ef71c43c97c251ecde655dbfd7de226
Coline ALBERTI <coline.alberti@gmail.com>
2020-12-07 09:17:49 +0100

    fonction getInt

--- js/alcoolemie.js
+++ js/alcoolemie.js
@@ -68,3 +68,20 @@ function getSanction(alcoolemie) {
         return '6 points + 2 ans de prison + suspension 3 ans + stage de sensibilisation';
     }
 }
+
+/**
+ * Fonction qui retourne une valeur entière récupérée via
+ * window.document.querySelector(id)
+ * 
+ * @param {string} id
+ * @return {integer}
+ */
+function getInt(id){
+    let valeur = parseInt(window.document.querySelector(id).value);
+    if (isNaN(valeur)){
+        window.document.querySelector(id).value = 0;
+        return 0;
+    } else{
+        return valeur;
+    }
+}
